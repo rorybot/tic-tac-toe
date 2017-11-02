004ad191f91347135e164c262af0792f54f10547
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 11:47:29 +0000

    GREEN: TicTacToe can declare a horizontal victory ;)

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,8 +1,15 @@
 
 class TicTacToe
-  attr_reader :player1
-  def initialize(player1 = 'Hero', player2 = 'Nemesis')
+  attr_reader :player1, :game_status, :board
+  def initialize(board = Board.new, player1 = 'Hero', player2 = 'Nemesis')
     @player1 = player1
     @player2 = player2
+    @game_status = nil
+    @board = board
+  end
+
+  def winner?
+    @game_status = 'Win for X' if board.horizontal_victory
+    return true
   end
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,16 +3,25 @@ require 'tictactoe'
 describe TicTacToe do
   describe 'initialisation' do
     it 'has a player 1 with a name' do
-      tictactoe = TicTacToe.new('Rory')
+      tictactoe = TicTacToe.new(Board.new, 'Rory')
       expect(tictactoe.player1).to eq 'Rory'
     end
   end
 
   describe 'playing a game' do
+
+    let(:board){ double('Board', horizontal_victory: true, game_status: 'Win for X')}
+
     it 'can declare a victory from a horizontal row' do
-      tictactoe = TicTacToe.new('Rory')
-      expect(tictactoe.horizontal_victory).to eq 'YAY! Three Xs in a row! You win!'
-      expect(tictactoe.game_status).to eq 'Win for Player 1'
+      tictactoe = TicTacToe.new(board)
+      expect(tictactoe.winner?).to eq true
+      expect(tictactoe.game_status).to eq 'Win for X'
+    end
+
+    it 'can declare a victory from a horizontal row' do
+      tictactoe = TicTacToe.new(board)
+      expect(tictactoe.winner?).to eq true
+      expect(tictactoe.game_status).to eq 'Win for X'
     end
   end
 end
