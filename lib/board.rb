fc61a1e3bebca616f61dc2e03b89bf3081c508f5
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 13:19:06 +0000

    Improved return value of victory check

--- lib/board.rb
+++ lib/board.rb
@@ -1,50 +1,51 @@
 class Board
   attr_reader :row_a, :row_b, :row_c, :game_status
   def initialize
-    @row_a = [nil,nil,nil]
-    @row_b = [nil,nil,nil]
-    @row_c = [nil,nil,nil]
+    @row_a = [nil, nil, nil]
+    @row_b = [nil, nil, nil]
+    @row_c = [nil, nil, nil]
     @game_status = nil
   end
 
   def place_symbol(symbol, row, index)
     raise 'Occupied' if row[index].class == String
     row[index] = symbol
-    return row
+    row
   end
 
   def victory?
-    check_every_horizontal_row_for_winner.class == String || check_every_vertical_row_for_winner.class == String
+    check_every_horizontal_row_for_winner.class == String || check_every_vertical_row_for_winner.class == String || check_every_diaganol_for_winner.class == String
   end
 
   def game_status
-     return @game_status = "Win for #{check_every_horizontal_row_for_winner}" if check_every_horizontal_row_for_winner
-     return @game_status = "Win for #{check_every_vertical_row_for_winner}" if check_every_vertical_row_for_winner
+    return @game_status = "Win for #{check_every_horizontal_row_for_winner}" if check_every_horizontal_row_for_winner
+    return @game_status = "Win for #{check_every_vertical_row_for_winner}" if check_every_vertical_row_for_winner
+    return @game_status = "Win for #{check_every_diaganol_for_winner}" if check_every_diaganol_for_winner
   end
 
   private
 
   def check_every_horizontal_row_for_winner
-    if row_a == ['X','X','X'] || row_a == ['O','O','O']
-      return row_a.first
-    elsif row_b == ['X','X','X'] || row_b == ['O','O','O']
-      return row_b.first
-    elsif row_c == ['X','X','X'] || row_c == ['O','O','O']
-      return row_c.first
+    if row_a == %w[X X X] || row_a == %w[O O O]
+      row_a.first
+    elsif row_b == %w[X X X] || row_b == %w[O O O]
+      row_b.first
+    elsif row_c == %w[X X X] || row_c == %w[O O O]
+      row_c.first
     end
   end
 
   def check_every_vertical_row_for_winner
     if row_a[0] && row_b[0] && row_c[0]
-      return row_a[0]
+      row_a[0]
     elsif row_a[1] && row_b[1] && row_c[1]
-      return row_a[1]
+      row_a[1]
     elsif row_a[2] && row_b[2] && row_c[2]
-      return row_a[2]
+      row_a[2]
     end
   end
 
   def check_every_diaganol_for_winner
-
+    row_a[0] && row_b[1] && row_c[2] || row_a[2] && row_b[1] && row_c[0]
   end
 end
