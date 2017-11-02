48ed097a0d835775f926b4031b4844b3f4e9ceaa
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 16:47:05 +0000

    RED: player tracks its turns

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,5 +5,10 @@ describe Player do
     it 'can place a piece' do
       expect(player.place_piece('X', board.row_a, 0)).to eq 'You have played a turn'
     end
+
+    it 'can track turns' do
+      player.place_piece('X', board.row_a, 0)
+      expect(player.turns).to eq 1
+    end
   end
 end
