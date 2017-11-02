a238b77cd2e47980ff268cc24c928ca78ee212ca
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 11:35:42 +0000

    RED: TicTacToe can declare a horizontal victory ;)

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -7,4 +7,12 @@ describe TicTacToe do
       expect(tictactoe.player1).to eq 'Rory'
     end
   end
+
+  describe 'playing a game' do
+    it 'can declare a victory from a horizontal row' do
+      tictactoe = TicTacToe.new('Rory')
+      expect(tictactoe.horizontal_victory).to eq 'YAY! Three Xs in a row! You win!'
+      expect(tictactoe.game_status).to eq 'Win for Player 1'
+    end
+  end
 end
