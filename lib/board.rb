4333a2c7b7aad13db2bd9b640a9db24efc6738c5
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 11:31:06 +0000

    Board can place symbols

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,13 @@
 class Board
   attr_reader :row_a, :row_b, :row_c
   def initialize
-    @row_a = []
-    @row_b = []
-    @row_c = []
+    @row_a = [nil,nil,nil]
+    @row_b = [nil,nil,nil]
+    @row_c = [nil,nil,nil]
+  end
+
+  def place_symbol(symbol, row, index)
+    row[index] = symbol
+    return row
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,16 @@
 describe Board do
   describe 'initialisation' do
+
+    let(:board){Board.new}
+
     it 'has three arrays' do
-      board = Board.new()
       expect(board.row_a.class).to eq Array
       expect(board.row_b.class).to eq Array
       expect(board.row_c.class).to eq Array
+    end
+
+    it 'can place things on the board' do
+      expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
     end
   end
 end
