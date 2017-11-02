8df54f750d3dc13d1dd639392c0a7016d813593c
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 11:20:36 +0000

    GREEN: Player1 exists with name

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -1,7 +1,7 @@
 
 class TicTacToe
-  def initialize
-
+  attr_reader :player1
+  def initialize(player1)
+    @player1 = player1
   end
-
 end
--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -3,8 +3,8 @@ require 'tictactoe'
 describe TicTacToe do
   describe 'initialisation' do
     it 'has a player 1 with a name' do
-      tictactoe = TicTacToe.new
-      expect(tictactoe.player1 = 'Rory')
+      tictactoe = TicTacToe.new('Rory')
+      expect(tictactoe.player1).to eq 'Rory'
     end
   end
 end
