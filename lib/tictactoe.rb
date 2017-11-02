2f99bb642c71a07383def1d1809b41a47d311eb1
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 13:18:32 +0000

    GREEN: Declare diaganol victory

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -4,12 +4,13 @@ class TicTacToe
   def initialize(board = Board.new, player1 = 'Hero', player2 = 'Nemesis')
     @player1 = player1
     @player2 = player2
-    @game_status = nil
+    @game_status = 'No Winner'
     @board = board
   end
 
   def query_game_status
     who_won? if winner?
+    return @game_status
   end
 
   private
@@ -20,6 +21,5 @@ class TicTacToe
 
   def who_won?
     @game_status = board.game_status if board.victory?
-    return game_status
   end
 end
