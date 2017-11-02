6c0f95d569ef442324850b99b3c3871c67c6c41d
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 16:48:28 +0000

    GREEN: player tracks its turns

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,11 @@
 class Player
+  attr_reader :turns
+  def initialize
+    @turns = 0
+  end
 
   def place_piece(symbol, row, index)
-    'You have played a turn'
+    @turns +=1
+    return 'You have played a turn'
   end
 end
