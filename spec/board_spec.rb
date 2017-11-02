03b8e2000c12ffdf64d445747a8ea38bb76f88fd
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 11:33:29 +0000

    Confirm a row can be filled

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,25 @@
 describe Board do
   describe 'initialisation' do
-
     let(:board){Board.new}
+      it 'has three arrays' do
+        expect(board.row_a.class).to eq Array
+        expect(board.row_b.class).to eq Array
+        expect(board.row_c.class).to eq Array
+      end
 
-    it 'has three arrays' do
-      expect(board.row_a.class).to eq Array
-      expect(board.row_b.class).to eq Array
-      expect(board.row_c.class).to eq Array
-    end
+  end
 
-    it 'can place things on the board' do
-      expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
-    end
+  describe 'place some pieces' do
+    let(:board){Board.new}
+      it 'can place things on the board' do
+        expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
+      end
+
+      it 'can place things on the board' do
+        expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
+        expect(board.place_symbol('X', board.row_a, 1)).to eq ['X', 'X', nil]
+        expect(board.place_symbol('X', board.row_a, 2)).to eq ['X', 'X', 'X']
+        expect(board.row_a).to eq ['X', 'X', 'X']
+      end
   end
 end
