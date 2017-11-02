ad70af9f70c641aad3d5eee42033638dcbf374eb
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 18:30:24 +0000

    Refactored down the letter checker

--- lib/board_logic.rb
+++ lib/board_logic.rb
@@ -27,31 +27,28 @@ class BoardLogic
   private
 
   def check_every_horizontal_row_for_winner
-    rows = [row_a, row_b, row_c]
-    rows.any? { |row| return row.first if every_letter_in_row_the_same?(row) }
+    row_query.any? { |row| return row.first if every_letter_the_same(row) }
   end
 
-  def every_letter_in_row_the_same?(row)
-    row.all? { |letter| letter == row[0] }
-  end
-
-  def every_letter_in_column_the_same(column)
-    column.all? { |letter| letter == column[0] }
+  def every_letter_the_same(line)
+    line.all?{|letter| letter == line[0]}
   end
 
   def check_every_vertical_row_for_winner
-    left_column = [row_a[0], row_b[0], row_c[0]]
-    middle_column = [row_a[1], row_b[1], row_c[1]]
-    right_column = [row_a[2], row_b[2], row_c[2]]
-    all_columns = left_column, middle_column, right_column
-    all_columns.any? { |column| return column[all_columns.index(column)] if every_letter_in_column_the_same(column) }
+    all_columns = row_query(0), row_query(1), row_query(2)
+    all_columns.any? { |column| return column[all_columns.index(column)] if every_letter_the_same(column) }
   end
 
   def check_every_diaganol_for_winner
     first_diagonal = [row_a[0], row_b[1], row_c[2]]
-    return row_b[1] if first_diagonal.all? { |letter| letter == first_diagonal[0] }
+    return row_b[1] if every_letter_the_same(first_diagonal)
 
     second_diagonal = [row_a[2], row_b[1], row_c[0]]
-    return row_b[1] if second_diagonal.all? { |letter| letter == second_diagonal[0] }
+    return row_b[1] if every_letter_the_same(second_diagonal)
+  end
+
+  def row_query(index = nil)
+    return [row_a[index], row_b[index], row_c[index]] if index
+    return [row_a, row_b, row_c]
   end
 end
