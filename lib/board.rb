f995df401811e6239b8ea3a52e1d9e03b4143e13
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-11-02 12:35:35 +0000

    Added logic excpected in TicTacToe to Board

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,34 @@
 class Board
-  attr_reader :row_a, :row_b, :row_c
+  attr_reader :row_a, :row_b, :row_c, :game_status
   def initialize
     @row_a = [nil,nil,nil]
     @row_b = [nil,nil,nil]
     @row_c = [nil,nil,nil]
+    @game_status = nil
   end
 
   def place_symbol(symbol, row, index)
     row[index] = symbol
     return row
+  end
+
+  def horizontal_victory?
+    return true if check_every_horizontal_row_for_winner == 'O' || 'X'
+  end
+
+  def game_status
+    return @game_status = "Win for #{check_every_horizontal_row_for_winner}" if horizontal_victory?
+  end
+
+  private
+
+  def check_every_horizontal_row_for_winner
+    if row_a == ['X','X','X'] || row_a == ['O','O','O']
+      return row_a.first
+    elsif row_b == ['X','X','X'] || row_b == ['O','O','O']
+      return row_b.first
+    elsif row_c == ['X','X','X'] || row_c == ['O','O','O']
+      return row_c.first
+    end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,25 +1,35 @@
 describe Board do
   describe 'initialisation' do
-    let(:board){Board.new}
-      it 'has three arrays' do
-        expect(board.row_a.class).to eq Array
-        expect(board.row_b.class).to eq Array
-        expect(board.row_c.class).to eq Array
-      end
-
+    let(:board) { Board.new }
+    it 'has three arrays' do
+      expect(board.row_a.class).to eq Array
+      expect(board.row_b.class).to eq Array
+      expect(board.row_c.class).to eq Array
+    end
   end
 
   describe 'place some pieces' do
-    let(:board){Board.new}
-      it 'can place things on the board' do
-        expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
-      end
+    let(:board) { Board.new }
+    it 'can place things on the board' do
+      expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
+    end
 
-      it 'can place things on the board' do
-        expect(board.place_symbol('X', board.row_a, 0)).to eq ['X', nil, nil]
-        expect(board.place_symbol('X', board.row_a, 1)).to eq ['X', 'X', nil]
-        expect(board.place_symbol('X', board.row_a, 2)).to eq ['X', 'X', 'X']
-        expect(board.row_a).to eq ['X', 'X', 'X']
-      end
+    it 'can place things on the board' do
+      (0..2).to_a.each { |i| board.place_symbol('X', board.row_a, i) }
+      expect(board.row_a).to eq %w[X X X]
+    end
+
+    it 'can declare horizontal_victory for X' do
+      (0..2).to_a.each { |i| board.place_symbol('X', board.row_a, i) }
+      expect(board.horizontal_victory?).to eq true
+      expect(board.game_status).to eq 'Win for X'
+    end
+
+    it 'can declare horizontal_victory for O' do
+      (0..2).to_a.each { |i| board.place_symbol('O', board.row_a, i) }
+      expect(board.horizontal_victory?).to eq true
+      expect(board.game_status).to eq 'Win for O'
+    end
+
   end
 end
